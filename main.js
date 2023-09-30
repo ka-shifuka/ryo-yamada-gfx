20a10834261270e010d5fc4f2e836f659f3990d3
ka-shifuka <aryadewanggapuja@gmail.com>
2023-09-30 08:18:18 +0700

    Add gallery

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-function getQuote() {
-  fetch("https://api.quotable.io/random")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      swal.fire({ text: data.content });
-    });
-}
-
-/*
- * theme toggler
- */
-const themeToggler = document.querySelector("#theme-toggler");
-const icon = themeToggler.querySelector("i");
-
-let isDark = false;
-themeToggler.addEventListener("click", () => {
-  const html = document.querySelector("html");
-  const browserBar = document.querySelector("#browser-bar");
-
-  isDark = !isDark;
-  if (isDark) {
-    html.classList.add("dark");
-    browserBar.content = "#202020";
-    icon.classList.remove("bi-moon-stars");
-    icon.classList.add("bi-sun");
+window.addEventListener("DOMContentLoaded", function () {
+  function getQuote() {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        swal.fire({ text: data.content });
+      });
   }
-  if (!isDark) {
-    html.classList.remove("dark");
-    browserBar.content = "#ffffff";
-    icon.classList.remove("bi-sun");
-    icon.classList.add("bi-moon-stars");
-  }
+
+  /*
+   * theme toggler
+   */
+  const themeToggler = document.querySelector("#theme-toggler");
+  const icon = themeToggler.querySelector("i");
+
+  let isDark = false;
+  themeToggler.addEventListener("click", () => {
+    const html = document.querySelector("html");
+    const browserBar = document.querySelector("#browser-bar");
+
+    isDark = !isDark;
+    if (isDark) {
+      html.classList.add("dark");
+      browserBar.content = "#202020";
+      icon.classList.remove("bi-moon-stars");
+      icon.classList.add("bi-sun");
+    }
+    if (!isDark) {
+      html.classList.remove("dark");
+      browserBar.content = "#ffffff";
+      icon.classList.remove("bi-sun");
+      icon.classList.add("bi-moon-stars");
+    }
+  });
+
 });
-
-// nav system
